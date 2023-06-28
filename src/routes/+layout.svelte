154aae950e0cda8823bab763fb32471b73d125a1
Nick Ng <nicholas.cj.ng@gmail.com>
2023-06-28 22:47:56 +1200

    feat: add tailwindcss. use static renderer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex flex-row justify-start">
+	<nav class="flex flex-col mr-2">
+		<a href="/">Home</a>
+		<a href="/writer">Writer</a>
+	</nav>
+
+	<slot />
+</div>
